8485942ea22e7c7b43b3df5f1bf4b3994bc9c4cd
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2021-08-01 18:05:12 -0400

    control bottom

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace igl
 {
-    partial class Form1
+    partial class igl
     {
         /// <summary>
         /// Required designer variable.
@@ -29,13 +29,41 @@ namespace igl
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(igl));
+            this.controllPanel = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // controllPanel
+            // 
+            this.controllPanel.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.controllPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(66)))), ((int)(((byte)(66)))), ((int)(((byte)(66)))));
+            this.controllPanel.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.controllPanel.Location = new System.Drawing.Point(0, 347);
+            this.controllPanel.Name = "controllPanel";
+            this.controllPanel.Size = new System.Drawing.Size(900, 103);
+            this.controllPanel.TabIndex = 0;
+            // 
+            // igl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(51)))));
+            this.ClientSize = new System.Drawing.Size(900, 450);
+            this.Controls.Add(this.controllPanel);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "igl";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Inital Game Launcher";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel controllPanel;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace igl
 {
-    public partial class Form1 : Form
+    public partial class igl : Form
     {
-        public Form1()
+        public igl()
         {
             InitializeComponent();
         }
